19efb61dc24628c5bec0cb97805d82e5ec79bc0b
Domas Markevičius <domasm@wix.com>
2017-10-28 17:25:55 +0200

    Trims autoRenames

--- shorten-symbol-names.sketchplugin/Contents/Sketch/autoRename.js
+++ shorten-symbol-names.sketchplugin/Contents/Sketch/autoRename.js
@@ -13,7 +13,7 @@ var onSelectionChanged = function(context) {
    if (symbolInserted && selection.length == 1 && selection[0].class() == "MSSymbolInstance") {
       var selectionName = selection[0].name();
       selectionName = selectionName.split("/");
-      selectionName = selectionName[selectionName.length - 1];
+      selectionName = selectionName[selectionName.length - 1].trim();
       selection[0].setName(selectionName);
       symbolInserted = false;
    }
